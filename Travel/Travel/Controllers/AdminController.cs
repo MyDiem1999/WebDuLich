2812e5b9e0e8da5d887b1981bdf8bfea77b05a96
HoaiSang <nguyenhoaisang1001@gmail.com>
2021-01-08 15:29:09 +0700

    controllerAdmin

---
+++ Travel/Travel/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Travel.Models;
+
+namespace Travel.Controllers
+{
+    public class AdminController : Controller
+    {
+        WEB_DULICHDataContext dl = new WEB_DULICHDataContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
